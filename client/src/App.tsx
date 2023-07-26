0eed448d16218347d505cf6a86793e97541e330f
Gonzalo Silvera <gonzalosilvera@gmail.com>
2023-07-25 23:45:00 -0300

    App Component Styling Changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
+import './App.css'
+
 function App() {
   return (
     <div className='flex items-center h-screen justify-center'>
-      <div className=''>
         <h1 className='text-3xl'>To Do App</h1>
-      </div>
     </div>
   )
 }
